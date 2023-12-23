703fd35a4631287ffc487ceaad91c37f2564fe10
Kinshuk Dewari <71977053+kinshuk-dewari@users.noreply.github.com>
2023-12-24 02:05:14 +0530

    Update index.tsx
    added achievements

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,16 +8,14 @@ import React from 'react'
 import Skills from '../components/Skills'
 import Projects from '../components/Projects'
 import ContactMe from '../components/ContactMe'
-import Link from 'next/link'
-import Image from 'next/image';
 import Socials from '../components/Socials'
 import Footer from '../components/Footer'
 import ScrollTop from '../components/ScrollTop'
+import Achievements from '../components/Achievements'
 
 const Home: NextPage = () => {
 
-  
-
+ 
   return (
     <div className='bg-[#0A0A0A] text-white h-screen snap-y snap-mandtory overflow-y-scroll 
     z-0 overflow-x-hidden scrollbar-thin scrollbar-thumb-rounded-md scrollbar-track-gray-400/20 scrollbar-thumb-[#E2711E]/80'>
@@ -28,12 +26,12 @@ const Home: NextPage = () => {
       
       <Header/>
       {/* Hero Section */} 
-      <section id="hero" >
+      <section id="hero">
         <Hero/>
       </section>
 
       {/* About Section*/}
-      <section id="about" >
+      <section id="about">
         <About/>
       </section>
 
@@ -45,22 +43,28 @@ const Home: NextPage = () => {
       <section id="projects" className="snap-center">
         <Projects/>
       </section>      
-
+       {/* Achievements Section  */}
+      <section id="achievements" className="snap-center">
+          <Achievements/>  
+      </section>
       {/* Experience Section */}
       <section id="experience" className="snap-center">
         <WorkExperience/>
       </section>
 
       {/* Skills Section */}
-      <section id="skills" className="snap-start">
+      <section id="skills">
         <Skills/>
       </section>
 
       {/* Contact Me Section */}
-      <section id="contact" >
+      <section id="contact" className="snap-start" >
         <ContactMe/>
       </section>
+      
       <ScrollTop/>
+
+      {/* Footer Section */}
       <Footer/>          
       
     </div>
