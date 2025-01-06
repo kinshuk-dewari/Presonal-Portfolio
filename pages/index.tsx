4657d347235d3b8566289aebb5befd42da35f0fd
Kinshuk Dewari <71977053+kinshuk-dewari@users.noreply.github.com>
2025-01-06 20:10:41 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,8 @@ const Home: NextPage = () => {
     <div className='bg-[#0A0A0A] text-white h-screen overflow-y-scroll 
     z-0 overflow-x-hidden scrollbar-thin scrollbar-thumb-rounded-md scrollbar-track-gray-400/20 scrollbar-thumb-[#E2711E]/80'>
       <Head>
-      <link rel="icon" href="/logo.png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/logo.png" />
         <title>Kinshuk's Portfolio</title>
       </Head>     
       
